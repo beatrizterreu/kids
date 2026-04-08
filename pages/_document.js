19df732cd1576a3f3dd4b7bf7a6a7ce6dfeca6e0
beatrizterreu <bterreu@gmail.com>
2026-04-09 01:04:42 +0200

    Add files via upload

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html translate="no">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9787997197025799"
+          crossOrigin="anonymous"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
